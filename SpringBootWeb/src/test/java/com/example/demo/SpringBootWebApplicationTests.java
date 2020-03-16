9c509fa7ec3cfb36026e711684cc9f7ee733a313
jrsm <1203351437@qq.com>
2020-03-16 11:22:09 +0800

    合并之后的

--- SpringBootWeb/src/test/java/com/example/demo/SpringBootWebApplicationTests.java
+++ SpringBootWeb/src/test/java/com/example/demo/SpringBootWebApplicationTests.java
@@ -1,10 +1,6 @@
 package com.example.demo;
 /*
-<<<<<<< HEAD
-abc
-=======
-12345
->>>>>>> branch 'master' of https://github.com/creator-jrsm/eclipseGitRemote.git
+abc+12345
 */
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
